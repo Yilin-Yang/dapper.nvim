938567dc404157ea6fba104463615d1625358777
Yilin Yang <yiliny@umich.edu>
2018-11-24 04:27:52 -0500

    Successfully obtain stack frame info from DA!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {DebugClient} from 'vscode-debugadapter-testsupport';
+import {DebugProtocol} from 'vscode-debugprotocol';
 
 console.log('Hello, World!');
 
@@ -17,17 +18,47 @@ dc.start()
       return dc.initializeRequest();
     })
     .then(response => {
-      console.log('foo');
+      console.log('Received initialization response!');
       console.log(response.body);
     });
 
 dc.waitForEvent('initialized')
     .then(response => {
-      console.log('Received initialize response.');
-      return dc.launch(
-          {'program': '/home/yiliny/plugin/dapper.nvim/build/src/index.js'});
+      console.log('Received initialized event.');
+      console.log('Reporting that configuration is done.');
+      return dc.configurationDoneRequest();
     })
     .then(response => {
-      console.log('Launched program.');
+      console.log('Initial configuration complete!');
+      console.log(response.body);
+      return dc.launch({
+        program: '/home/yiliny/plugin/dapper.nvim/README.md',
+        stopOnEntry: true
+      });
+    })
+    .then(response => {
+      console.log('Launched program!');
       console.log(response.body);
     });
+
+// Program should immediately hit a breakpoint
+dc.waitForEvent('stopped')
+    .then(response => {
+      console.log('Stopped execution.');
+      console.log(
+          'stop reason: ' + response.body.reason + '\n' +
+          'thread id: ' + response.body.threadId);
+      return dc.stackTraceRequest({threadId: response.body.threadId});
+    })
+    .then(response => {
+      console.log('Obtained stack frame information!');
+      console.log(response.body.stackFrames);
+    });
+
+// })
+// .then(response => {
+//   console.log('Launched program.');
+//   return dc.setBreakpointsRequest(
+//       {'source': Source.path('/home/yiliny/plugin/dapper.nvim/README.md'),
+//        'breakpoints': [1,2,3]});
+// });
