a7c74a4a3fa91f075cc98bbd84670c4c84c31329
Yilin Yang <yiliny@umich.edu>
2018-11-24 02:28:10 -0500

    Run `npx gts fix`, adjust style

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,32 @@ import {DebugClient} from 'vscode-debugadapter-testsupport';
 
 console.log('Hello, World!');
 
-let dc : DebugClient;
+let dc: DebugClient;
 
-// dc = new DebugClient('node', './build/vscode-mock-debug/out/debugAdapter.js', 'mock');
-dc = new DebugClient('node', '/home/yiliny/plugin/dapper.nvim/build/vscode-mock-debug/out/debugAdapter.js', 'mock');
+// dc = new DebugClient('node', './build/vscode-mock-debug/out/debugAdapter.js',
+// 'mock');
+dc = new DebugClient(
+    'node',
+    '/home/yiliny/plugin/dapper.nvim/build/vscode-mock-debug/out/debugAdapter.js',
+    'mock');
 
-dc.start().then(response => {
-  console.log('Debug adapter started!');
-  return dc.initializeRequest();
-}).then(response => {
-  console.log('foo');
-  console.log(response.body);
-});
+dc.start()
+    .then(response => {
+      console.log('Debug adapter started!');
+      return dc.initializeRequest();
+    })
+    .then(response => {
+      console.log('foo');
+      console.log(response.body);
+    });
 
-dc.waitForEvent('initialized').then(response => {
-  console.log('Received initialize response.');
-  return dc.launch({'program': '/home/yiliny/plugin/dapper.nvim/build/src/index.js'});
-}).then(response => {
-  console.log('Launched program.');
-  console.log(response.body);
-});
+dc.waitForEvent('initialized')
+    .then(response => {
+      console.log('Received initialize response.');
+      return dc.launch(
+          {'program': '/home/yiliny/plugin/dapper.nvim/build/src/index.js'});
+    })
+    .then(response => {
+      console.log('Launched program.');
+      console.log(response.body);
+    });
