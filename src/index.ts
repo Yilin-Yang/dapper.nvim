409a8356ad2c8567939102b3c1c71b7fd3de3be2
Yilin Yang <yiliny@umich.edu>
2018-11-24 02:24:32 -0500

    Successfully initialize mock debug adapter!

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import {DebugClient} from 'vscode-debugadapter-testsupport';
 
-console.log('Hello, World!');
+console.error('Hello, World!');
 
 let dc : DebugClient;
 
-dc = new DebugClient('node', './build/vscode-mock-debug/out/debugAdapter.js', 'mock');
+// dc = new DebugClient('node', './build/vscode-mock-debug/out/debugAdapter.js', 'mock');
+dc = new DebugClient('node', '/home/yiliny/plugin/dapper.nvim/build/vscode-mock-debug/out/debugAdapter.js', 'mock');
 
-dc.start().then((res) => {
-  console.log('Debug adapter started!');
-  console.log('Trying initialization...');
+dc.start().then(response => {
+  console.error('Debug adapter started!');
   return dc.initializeRequest();
-}).then((res) => {
-  console.log('Debug adapter initialized!');
-  console.log(res);
-}).catch((rej) => {
-  console.log('Initialization request failed!');
-  throw new Error(rej);
+}).then(response => {
+  console.error('foo');
+  console.error(response.body);
+});
+
+dc.waitForEvent('initialized').then(response => {
+  console.error('Received initialize response.');
+  return dc.launch({'program': '/home/yiliny/plugin/dapper.nvim/build/src/index.js'});
+}).then(response => {
+  console.error('Launched program.');
+  console.error(response.body);
 });
