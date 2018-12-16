1fde9bce58d0801583a92ef9e380d13d1a82da6f
Yilin Yang <yiliny@umich.edu>
2018-12-16 05:08:00 -0500

    Add workaround for broken :UpdateRemotePlugins

---
+++ update_remote_plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# EFFECTS:  "Register" plugin commands in the remote plugin manifest.
+# DETAILS:  Workaround for broken Node.js remote plugin registration.
+
+MANIFEST="$HOME/.local/share/nvim/rplugin.vim"
+
+# strip empty remote plugin registration, if present
+nvim -n -u NONE -i NONE --headless "$MANIFEST" -c "%s/\n^.\{-}dapper.nvim'\_.\{-}).\{-}//e" -cwq
+
+cat << EOF >> "$MANIFEST"
+call remote#host#RegisterPlugin('node', '/home/yiliny/plugin/dapper.nvim/rplugin/node/dapper.nvim', [
+      \ {'sync': v:false, 'name': 'DapperStart', 'type': 'command', 'opts': {'nargs': '+'}},
+     \ ])
+EOF
