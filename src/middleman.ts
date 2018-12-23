ce482f2904c9b9d3b8f43128442fdf69fe224d0f
Yilin Yang <yiliny@umich.edu>
2018-12-23 04:17:52 -0500

    Add untested 'start adapter' method to Middleman

--- src/middleman.ts
+++ src/middleman.ts
@@ -1,17 +1,62 @@
 import {Neovim, NvimPlugin} from 'neovim';
-// import {DebugProtocol} from 'vscode-debugprotocol';
+import {DebugClient} from 'vscode-debugadapter-testsupport';
+import {DebugProtocol} from 'vscode-debugprotocol';
 
 /**
  * The middleman between dapper's VimL frontend and the debug adapter backend.
  */
 class Middleman {
+  static readonly CLIENT_NAME: string = 'dapper.nvim';
+
+  private static readonly EMPTY_DC: DebugClient = {} as DebugClient;
+
   /**
    * For manipulating the user-facing neovim instance.
    */
   private nvim: Neovim;
+  private dc: DebugClient;
+  private capabilities: DebugProtocol.Capabilities;
 
   constructor(api: NvimPlugin) {
     this.nvim = api.nvim;
+    this.dc = Middleman.EMPTY_DC;
+    this.capabilities = {};
+  }
+
+  /**
+   * Start a debug adapter.
+   *
+   * See DebugClient for additional parameter documentation.
+   * @param   runtimeEnv  The environment in which to run the debug adapter,
+   *                      e.g. `python`, `node`.
+   * @param   exeFilepath The filepath to the debug adapter executable.
+   * @param   adapterID   The name of the debug adapter.
+   * @returns {} `true` when the initialization succeeded, `false` otherwise.
+   */
+  startAdapter(runtimeEnv: string, exeFilepath: string, adapterID: string): Promise<boolean> {
+      return new Promise<boolean>(async (resolve, reject) => {
+        try {
+          // TODO: if dc != EMPTY_DC, terminate the still running process
+          this.dc = new DebugClient(runtimeEnv, exeFilepath, adapterID);
+          const args: DebugProtocol.InitializeRequestArguments = {
+            clientName: Middleman.CLIENT_NAME,
+            adapterID,
+            linesStartAt1: true,
+            columnsStartAt1: true,
+            // TODO support the items below
+            // supportsVariableType: true,
+            // supportsVariablePaging: true,
+            // supportsRunInTerminalRequest: true,
+          };
+          const response: DebugProtocol.InitializeResponse =
+            await this.dc.initializeRequest(args);
+          this.capabilities = response.body as DebugProtocol.Capabilities;
+          resolve(true);
+        } catch (e) {
+          // TODO: log exception
+          resolve(false);
+        }
+      });
   }
 
   success(): Promise<string> {
