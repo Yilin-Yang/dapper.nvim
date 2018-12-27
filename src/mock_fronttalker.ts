78e3fae5524b3cc26cecd31094bfbd5025fa921d
Yilin Yang <yiliny@umich.edu>
2018-12-27 03:13:23 -0500

    Create MockFrontTalker type

---
+++ src/mock_fronttalker.ts
@@ -0,0 +1,23 @@
+import {BasicFrontTalker} from './basic_fronttalker';
+import { DapperAnyMsg } from './messages';
+
+/**
+ * FrontTalker implementation for use in test cases.
+ */
+export class MockFrontTalker extends BasicFrontTalker {
+  private lastMessage: DapperAnyMsg | undefined;
+
+  constructor() {
+    super();
+    this.lastMessage = undefined;
+  }
+
+  send(msg: DapperAnyMsg): void {
+    // deep copy outgoing message
+    this.lastMessage = JSON.parse(JSON.stringify(msg));
+  }
+
+  getLast(): DapperAnyMsg | undefined {
+    return JSON.parse(JSON.stringify(this.lastMessage));
+  }
+}
