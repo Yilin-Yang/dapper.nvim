b3e4c14d6402493e88781d4c806cec18d8f1c09b
Yilin Yang <yiliny@umich.edu>
2018-12-27 01:41:40 -0500

    Write and pass FrontTalker regex test cases

--- test/test_basic_fronttalker.ts
+++ test/test_basic_fronttalker.ts
@@ -113,6 +113,24 @@ describe('BasicFrontTalker, with string typenames only,', () => {
     assert.deepEqual(subArr[1].getLastAndReset(), {});
     assert.deepEqual(subArr[2].getLastAndReset(), {});
   });
+  it('correctly removes, then restores subscribers', () => {
+    const testObj = new BasicFrontTalker();
+    const subArr =
+        [new MockSubscriber(), new MockSubscriber(), new MockSubscriber()];
+    for (let i = 0; i < subArr.length; ++i) {
+      testObj.on('initialize', subArr[i].receive);
+    }
+    testObj.off('initialize', subArr[0].receive);
+    testObj.off('initialize', subArr[1].receiveWrong.bind(subArr[1]));
+    testObj.off('initialize', subArr[2].receive);
+    testObj.on('initialize', subArr[2].receive);
+
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+
+    assert.deepEqual(subArr[0].getLastAndReset(), {});
+    assert.deepEqual(subArr[1].getLastAndReset(), {});
+    assert.deepEqual(subArr[2].getLastAndReset(), initializeRequest);
+  });
 });
 
 describe('BasicFrontTalker, with regex subscriptions only,', () => {
@@ -124,5 +142,118 @@ describe('BasicFrontTalker, with regex subscriptions only,', () => {
     assert.deepEqual(sub.getLastAndReset(), initializeRequest);
     assert.equal(testObj.emit('launch', launchRequest), true);
     assert.deepEqual(sub.getLastAndReset(), launchRequest);
+  });
+  it('only calls back when regex matches', () => {
+    const testObj = new BasicFrontTalker();
+    const subInit = new MockSubscriber();
+    const subLaunch = new MockSubscriber();
+    testObj.on(new RegExp('ini'), subInit.receive);
+    testObj.on(new RegExp('[lL]au'), subLaunch.receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(subInit.getLastAndReset(), initializeRequest);
+    assert.equal(testObj.emit('launch', launchRequest), true);
+    assert.deepEqual(subLaunch.getLastAndReset(), launchRequest);
+  });
+  it('correctly calls back multiple subscribers with identical regex', () => {
+    const testObj = new BasicFrontTalker();
+    const sub1 = new MockSubscriber();
+    const sub2 = new MockSubscriber();
+    const sub3 = new MockSubscriber();
+    testObj.on(new RegExp('ini'), sub1.receive);
+    testObj.on(new RegExp('ini'), sub2.receive);
+    testObj.on(new RegExp('[lL]au'), sub2.receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(sub1.getLastAndReset(), initializeRequest);
+    assert.deepEqual(sub2.getLastAndReset(), initializeRequest);
+    assert.deepEqual(sub3.getLastAndReset(), {});
+  });
+  it('correctly calls back multiple subscribers with equivalent regex', () => {
+    const testObj = new BasicFrontTalker();
+    const sub1 = new MockSubscriber();
+    const sub2 = new MockSubscriber();
+    const sub3 = new MockSubscriber();
+    testObj.on(new RegExp('ini'), sub1.receive);
+    testObj.on(new RegExp('.*'), sub2.receive);
+    testObj.on(new RegExp('zzz'), sub2.receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(sub1.getLastAndReset(), initializeRequest);
+    assert.deepEqual(sub2.getLastAndReset(), initializeRequest);
+    assert.deepEqual(sub3.getLastAndReset(), {});
+  });
+  it('correctly removes subscribers in order', () => {
+    const testObj = new BasicFrontTalker();
+    const subs = [
+      new MockSubscriber(), new MockSubscriber(), new MockSubscriber(),
+      new MockSubscriber()
+    ];
+    testObj.on(new RegExp('ini'), subs[0].receive);
+    testObj.on(new RegExp('ini'), subs[1].receive);
+    testObj.on(new RegExp('ini'), subs[2].receive);
+    testObj.on(new RegExp('ini'), subs[3].receive);
+    testObj.off(new RegExp('ini'), subs[0].receive);
+    testObj.off(new RegExp('ini'), subs[3].receive);  // should remove subs[2]
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(subs[0].getLastAndReset(), {});
+    assert.deepEqual(subs[1].getLastAndReset(), {});
+    assert.deepEqual(subs[2].getLastAndReset(), initializeRequest);
+    assert.deepEqual(subs[3].getLastAndReset(), initializeRequest);
+  });
+  it('correctly removes all', () => {
+    const testObj = new BasicFrontTalker();
+    const subs = [
+      new MockSubscriber(), new MockSubscriber(), new MockSubscriber(),
+      new MockSubscriber()
+    ];
+    testObj.on(new RegExp('ini'), subs[0].receive);
+    testObj.on(new RegExp('ini'), subs[1].receive);
+    testObj.on(new RegExp('ini'), subs[2].receive);
+    testObj.on(new RegExp('ini'), subs[3].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), false);
+    assert.deepEqual(subs[0].getLastAndReset(), {});
+    assert.deepEqual(subs[1].getLastAndReset(), {});
+    assert.deepEqual(subs[2].getLastAndReset(), {});
+    assert.deepEqual(subs[3].getLastAndReset(), {});
+  });
+  it('correctly removes all and restores one', () => {
+    const testObj = new BasicFrontTalker();
+    const subs = [
+      new MockSubscriber(), new MockSubscriber(), new MockSubscriber(),
+      new MockSubscriber()
+    ];
+    testObj.on(new RegExp('ini'), subs[0].receive);
+    testObj.on(new RegExp('ini'), subs[1].receive);
+    testObj.on(new RegExp('ini'), subs[2].receive);
+    testObj.on(new RegExp('ini'), subs[3].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.off(new RegExp('ini'), subs[1].receive);
+    testObj.on(new RegExp('ini'), subs[1].receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(subs[0].getLastAndReset(), {});
+    assert.deepEqual(subs[1].getLastAndReset(), initializeRequest);
+    assert.deepEqual(subs[2].getLastAndReset(), {});
+    assert.deepEqual(subs[3].getLastAndReset(), {});
+  });
+  it('correctly removes one subscriber', () => {
+    const testObj = new BasicFrontTalker();
+    const sub = new MockSubscriber();
+    testObj.on(new RegExp('.*'), sub.receive);
+    testObj.off(new RegExp('.*'), sub.receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), false);
+    assert.deepEqual(sub.getLastAndReset(), {});
+  });
+  it('correctly removes and restores one subscriber', () => {
+    const testObj = new BasicFrontTalker();
+    const sub = new MockSubscriber();
+    testObj.on(new RegExp('.*'), sub.receive);
+    testObj.off(new RegExp('.*'), sub.receive);
+    testObj.on(new RegExp('.*'), sub.receive);
+    assert.equal(testObj.emit('initialize', initializeRequest), true);
+    assert.deepEqual(sub.getLastAndReset(), initializeRequest);
   });
 });
