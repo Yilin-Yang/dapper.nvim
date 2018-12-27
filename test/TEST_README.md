2b3314b22f975acea18e789800bde1518027f3f6
Yilin Yang <yiliny@umich.edu>
2018-12-27 05:08:36 -0500

    Use relative filepath to specify debug adapter
    Also, add a test README, for use with the mock runtime in test cases.

---
+++ test/TEST_README.md
@@ -0,0 +1,8 @@
+# Mock Debugger "Source File"
+
+This is a markdown file meant for use with [vscode-mock-debug](https://github.com/Microsoft/vscode-mock-debug)
+in test cases.
+
+Modifying this file will probably break middle-end test cases.
+
+The word `exception` causes the mock debugger to stop, due to an "exception".
--- test/test_middleman.ts
+++ test/test_middleman.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import {describe, it} from 'mocha';
+import * as path from 'path';
 
 import {DebugProtocol} from 'vscode-debugprotocol';
 
@@ -16,14 +17,16 @@ const mockCapabilities: DebugProtocol.Capabilities = {
     supportsEvaluateForHovers: true,
     supportsStepBack: true};
 
-describe('Middleman initialization', () => {
+describe('Middleman initialization, mock debug adapter', () => {
   it('can be constructed with a mock FrontTalker', () => {
     mm = new Middleman(ft);
   });
+
   it('can start a mock debug adapter', async () => {
     const result = await mm.startAdapter(
         'node',
-        '/home/yiliny/plugin/dapper.nvim/node_modules/vscode-mock-debug/out/debugAdapter.js',
+        path.join(__dirname, '..', 'node_modules', 'vscode-mock-debug',
+            'out', 'debugAdapter.js'),
         'mock');
     assert.equal(result, true);
     assert.deepEqual(mm.getCapabilities(), mockCapabilities);
@@ -35,7 +38,26 @@ describe('Middleman initialization', () => {
     return result;
   }).timeout(TIMEOUT_LEN);
 });
-describe('Middleman termination', () => {
+
+describe('Middleman interaction, mock debug adapter', () => {
+  it('can launch a "debuggee"', async () => {
+    // const launchRequestArgs = {
+    //   noDebug: false,
+    //   program: './TEST_README.md',
+    //   stopOnEntry: true
+    // };
+    // TODO: refine Middleman interface, only pass 'args' structs, not
+    // full-blown requests
+    // const result = await mm.request(launchRequestArgs);
+    const result = true;
+    return result;
+  });
+
+  // it('can retrieve a list of all active threads', async () => {
+  // });
+});
+
+describe('Middleman termination, mock debug adapter', () => {
   it('can terminate the running debug adapter and debuggee process', async () => {
      const result = await mm.terminate();
      return result;
