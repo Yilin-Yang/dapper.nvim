670830f454495e057a51fd383b1d89d8f1760159
Yilin Yang <yiliny@umich.edu>
2018-12-28 22:12:38 -0500

    Have MockFrontTalker store all forwarded messages
    So that certain function calls, like `dapper.startAndConfigure`, won't
    clobber MockFrontTalker's "last message received" (e.g. overwriting an
    `InitializeResponse` with a `ConfigurationDoneResponse`).

--- src/mock_fronttalker.ts
+++ src/mock_fronttalker.ts
@@ -5,19 +5,37 @@ import {DapperAnyMsg} from './messages';
  * FrontTalker implementation for use in test cases.
  */
 export class MockFrontTalker extends BasicFrontTalker {
-  private lastMessage: DapperAnyMsg|undefined;
+  private messages: DapperAnyMsg[]|undefined;
 
   constructor() {
     super();
-    this.lastMessage = undefined;
+    this.messages = undefined;
   }
 
   send(msg: DapperAnyMsg): void {
     // deep copy outgoing message
-    this.lastMessage = JSON.parse(JSON.stringify(msg));
+    if (!this.messages) {
+      this.messages = [];
+    }
+    this.messages.push(JSON.parse(JSON.stringify(msg)));
   }
 
   getLast(): DapperAnyMsg|undefined {
-    return JSON.parse(JSON.stringify(this.lastMessage));
+    if (!this.messages) return undefined;
+    return JSON.parse(JSON.stringify(this.messages[this.messages.length - 1]));
+  }
+
+  hasReceived(typename: string): boolean {
+    if (!this.messages) return false;
+    const msgs = this.messages;
+    for (let i = 0; i < msgs.length; ++i) {
+      const msg = msgs[i];
+      if (msg.vim_msg_typename === typename) return true;
+    }
+    return false;
+  }
+
+  reset(): void {
+    this.messages = undefined;
   }
 }
