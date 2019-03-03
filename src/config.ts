2b28a7e7060b642ae186de90b98d845019360dfe
Yilin Yang <yiliny@umich.edu>
2019-03-02 23:52:53 -0500

    debugServer? is a (port) number, not a string
    I'm pretty sure; TODO double-check this.

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ export interface VSCodeAttributes {
   preLaunchTask?: string;
   postLaunchTask?: string;
   internalConsoleOptions?: string;
-  debugServer?: string;
+  debugServer?: number;
 }
 
 export interface StartArgs {
