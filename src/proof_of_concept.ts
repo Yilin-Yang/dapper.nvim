266c6c1bb3eb114fa1f68603f1a213fdf2393fb6
Yilin Yang <yiliny@umich.edu>
2019-05-16 03:25:47 -0400

    clang-format proof of concept code

--- src/proof_of_concept.ts
+++ src/proof_of_concept.ts
@@ -21,31 +21,35 @@ async function setup() {
       'node2');
   dc.start();
   dc.configurationSequence().then(response => {
-      console.log(response);
-    });
+    console.log(response);
+  });
   dc.waitForEvent('stopped').then(event => {
-      console.log(event);
-    });
-  await dc.launch({
-      program: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js',
-      stopOnEntry: true})
-    .then(response => {
-          console.log(response);
+    console.log(event);
+  });
+  await dc
+      .launch({
+        program: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js',
+        stopOnEntry: true
+      })
+      .then(response => {
+        console.log(response);
       });
-  await dc.threadsRequest().then(response => {
-      console.log(response.body.threads);
-      return dc.stackTraceRequest({threadId: response.body.threads[0].id});
-    }).then(response => {
-      console.log(JSON.stringify(response));
-    });
+  await dc.threadsRequest()
+      .then(response => {
+        console.log(response.body.threads);
+        return dc.stackTraceRequest({threadId: response.body.threads[0].id});
+      })
+      .then(response => {
+        console.log(JSON.stringify(response));
+      });
   // console.log(await dc.hitBreakpoint(
   //   { program: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js' },
-  //   { path: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js', line: 1 },
-  //   { line: 1 })
+  //   { path: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js', line: 1
+  //   }, { line: 1 })
   // );
   dc.stop().then(response => {
-      console.log(response);
-    });
+    console.log(response);
+  });
 }
 
 setup();
