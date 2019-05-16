25094173dc31cba6e1e65eafd1106009730e914e
Yilin Yang <yiliny@umich.edu>
2019-05-15 23:28:10 -0400

    Get proof of concept to do Anything At All

--- src/proof_of_concept.ts
+++ src/proof_of_concept.ts
@@ -1,65 +1,21 @@
 import {DebugClient} from 'vscode-debugadapter-testsupport';
-import {DebugProtocol} from 'vscode-debugprotocol';
 
 console.log('Hello, World!');
 
 let dc: DebugClient;
 
-dc = new DebugClient(
-    'node',
-    '/home/yiliny/plugin/dapper.nvim/node_modules/vscode-mock-debug/out/debugAdapter.js',
-    'mock');
+async function setup() {
+  dc = new DebugClient(
+      'node',
+      '/home/yiliny/plugin/dapper.nvim/extensions/vscode-node-debug2/out/src/nodeDebug.js',
+      'node2');
+  dc.start();
+  console.log(await dc.hitBreakpoint(
+    { program: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js' },
+    { path: '/home/yiliny/plugin/dapper.nvim/test/js_test/index.js', line: 1 },
+    { line: 1 })
+  );
+  dc.stop();
+}
 
-dc.start()
-    .then(response => {
-      console.log('Debug adapter started!');
-      return dc.initializeRequest();
-    })
-    .then(response => {
-      console.log('Received initialization response!');
-      console.log(response.body);
-    });
-
-dc.waitForEvent('initialized')
-    .then(response => {
-      console.log('Received initialized event.');
-      console.log('Reporting that configuration is done.');
-      return dc.configurationDoneRequest();
-    })
-    .then(response => {
-      console.log('Initial configuration complete!');
-      console.log(response.body);
-      return dc.launch({
-        program: '/home/yiliny/plugin/dapper.nvim/test/TEST_README.md',
-        stopOnEntry: true
-      });
-    })
-    .then(response => {
-      console.log('Launched program!');
-      console.log(response.body);
-    });
-
-// Program should immediately hit a breakpoint
-dc.waitForEvent('stopped')
-    .then(response => {
-      console.log('Stopped execution.');
-      console.log(
-          'stop reason: ' + response.body.reason + '\n' +
-          'thread id: ' + response.body.threadId);
-      return dc.threadsRequest();
-      // return dc.stackTraceRequest({threadId: response.body.threadId});
-    })
-    .then(response => {
-      console.log('Obtained threads information!');
-      console.log(response.body.threads);
-      // console.log('Obtained stack frame information!');
-      // console.log(response.body.stackFrames);
-    });
-
-// })
-// .then(response => {
-//   console.log('Launched program.');
-//   return dc.setBreakpointsRequest(
-//       {'source': Source.path('/home/yiliny/plugin/dapper.nvim/README.md'),
-//        'breakpoints': [1,2,3]});
-// });
+setup();
